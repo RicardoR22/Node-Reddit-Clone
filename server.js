ba6eda9b60b9cbf1eee58eadf31a1434abfaa0f8
Ricardo Rodriguez <rr092293@gmail.com>
2019-02-13 00:11:29 -0800

    Users can sign up

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+const dotenv = require('dotenv').config();
 const express = require('express')
 const exphbs = require('express-handlebars');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const app = express();
 const post = require('./controllers/posts')(app);
 const comment = require('./controllers/comments.js')(app);
+const auth = require('./controllers/auth.js')(app);
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+
 
 // Set db
 require('./data/reddit-db');
@@ -17,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Add after body parser initialization!
 app.use(expressValidator());
+
+// USE COOKIE PARSER
+app.use(cookieParser());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
