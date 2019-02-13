bec7831e12b9893e1d87f025a7136973b17797a5
Ricardo Rodriguez <rr092293@gmail.com>
2019-02-12 22:17:50 -0800

    Users can see Subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -52,4 +52,15 @@ module.exports = function(app) {
         });
     });
 
+    // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+      Post.find({ subreddit: req.params.subreddit })
+        .then(posts => {
+          res.render("posts-index", { posts });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    });
+
 }
