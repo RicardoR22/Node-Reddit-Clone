3f96a19a5b8ffa9050c82bb8247398ef62eaf551
Ricardo Rodriguez <rr092293@gmail.com>
2019-02-12 21:30:07 -0800

    Users can submit and save posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,38 @@
 //posts.js
+const Post = require('../models/post');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
-// Index
-app.get('/', (req, res) => {
-    res.render('home', { msg: 'handlebars are cool'});
-})
+module.exports = function(app) {
 
-// New
-app.get('/posts/new', (req, res) => {
-    res.render('posts-new', {});
-})
+    // Use Body Parser
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    // Add after body parser initialization!
+    app.use(expressValidator());
+
+    
+    // Index
+    app.get('/', (req, res) => {
+        res.render('posts-index', { msg: 'handlebars are cool'});
+    })
+
+    // New
+    app.get('/posts/new', (req, res) => {
+        res.render('posts-new', {});
+    })
+
+    // CREATE
+    app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+    });
+
+}
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express();
+const post = require('./controllers/posts')(app);
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
-
+// Set db
+require('./data/reddit-db');
 
 const port = process.env.PORT || 4000;
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
